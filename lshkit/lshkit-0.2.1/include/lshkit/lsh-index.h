13a56afc3bf9279ef270049b49554ad42298c6cc
Conall <ocallaghan@gmail.com>
2014-04-09 23:29:05 -0400

    debugging

--- lshkit/lshkit-0.2.1/include/lshkit/lsh-index.h
+++ lshkit/lshkit-0.2.1/include/lshkit/lsh-index.h
@@ -167,6 +167,8 @@ public:
         for (unsigned i = 0; i < lshs_.size(); ++i) {
             unsigned index = lshs_[i](obj);
             BOOST_FOREACH(Key key, tables_[i][index]) {
+                std::cout << "CONALL" << std::endl;
+                std::cout << key << std::endl
                 scanner(key);
             }
         }
